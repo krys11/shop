9f728ea12b74860681e42be1334daa4441b69567
krys11 <krysdario1996@gmail.com>
2023-11-21 20:02:07 +0100

    all

--- shopApp/Screens/Cart/Cart.js
+++ shopApp/Screens/Cart/Cart.js
@@ -1,46 +1,72 @@
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-import React, { Fragment } from "react";
-import { List, Avatar } from "react-native-paper";
+import React, { Fragment, useState } from "react";
+import { List, Avatar, Button } from "react-native-paper";
 
 import { connect } from "react-redux";
 
 const Cart = (props) => {
+  let toTalPrice = 0;
+  props.cartItems.forEach((item) => {
+    return (toTalPrice += item.product.item.price);
+  });
+
   return (
     <>
       <Text style={styles.title}>Cart</Text>
-      <ScrollView>
-        {props.cartItems.length > 0 ? (
-          props.cartItems.map((it) => (
-            <View key={Math.random()} style={{ marginHorizontal: 20 }}>
-              <List.Item
-                title={
-                  <Text style={{ fontWeight: "bold" }}>
-                    {it.product.item.name}
-                  </Text>
-                }
-                left={() => (
-                  <Avatar.Image
-                    size={60}
-                    source={{
-                      uri: it.product.item.image
-                        ? it.product.item.image
-                        : "https://www.freepngimg.com/thumb/fifa/11-2-fifa-png-images.png",
-                    }}
-                  />
-                )}
-                right={() => (
-                  <Text style={styles.price}>$ {it.product.item.price}</Text>
-                )}
-              />
+      {props.cartItems.length > 0 ? (
+        <>
+          <ScrollView>
+            {props.cartItems.map((it) => (
+              <View key={Math.random()} style={{ marginHorizontal: 20 }}>
+                <List.Item
+                  title={
+                    <Text style={{ fontWeight: "bold" }}>
+                      {it.product.item.name}
+                    </Text>
+                  }
+                  left={() => (
+                    <Avatar.Image
+                      size={60}
+                      source={{
+                        uri: it.product.item.image
+                          ? it.product.item.image
+                          : "https://www.freepngimg.com/thumb/fifa/11-2-fifa-png-images.png",
+                      }}
+                    />
+                  )}
+                  right={() => (
+                    <Text style={styles.price}>$ {it.product.item.price}</Text>
+                  )}
+                />
+              </View>
+            ))}
+          </ScrollView>
+          <View style={styles.bottomContainer}>
+            <View>
+              <Text style={{ fontSize: 20, color: "blue" }}>
+                $ {toTalPrice}
+              </Text>
             </View>
-          ))
-        ) : (
-          <View style={styles.noProducts}>
-            <Text style={styles.noProductsText}>Your cart is empty</Text>
-            <Text>Add Product To Your To Get Started</Text>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ marginRight: 10 }}>
+                <Button mode="outlined" onPress={() => ""}>
+                  Clear
+                </Button>
+              </View>
+              <View>
+                <Button mode="outlined" onPress={() => ""}>
+                  CheckOut
+                </Button>
+              </View>
+            </View>
           </View>
-        )}
-      </ScrollView>
+        </>
+      ) : (
+        <View style={styles.noProducts}>
+          <Text style={styles.noProductsText}>Your cart is empty</Text>
+          <Text>Add Product To Your To Get Started</Text>
+        </View>
+      )}
     </>
   );
 };
@@ -72,6 +98,13 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     fontWeight: "bold",
     fontSize: 20,
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    paddingVertical: 15,
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 30,
   },
 });
 
